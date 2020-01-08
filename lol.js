48d232271446ad1f0ce96d4ad8b0f52434f908f1
Emil <eesenjanov@gmail.com>
2020-01-08 19:22:34 +0600

    first commit

---
+++ lol.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var leftBorderWidth = 1;
+{
+  let second = 2;
+  console.log(second);
+}
+const pi = 3.14;
+
+//
+
+var w124 = 1490;
+var w210 = 1990;
+var w140 = 2720;
+var w211 = 2075;
+var w220 = 2220;
+var w221 = 2260;
+var w222 = 2250;
+
+console.log(w124 + w210 + w211);
+console.log(w140 + w220 + w221 + w222);
+console.log(w124 + w140);
+
+
+// use comments
+
+/* if i have many words 
+   how can i comment it?*/
